e6674e8e402cb0a2a7ab4d5a089cd770f1d700ce
xiaozeze <chengjiatang679@126.com>
2025-04-29 10:20:15 +0800

    Update basic.tsx
    Signed-off-by: xiaozeze <chengjiatang679@126.com>

--- components/tooltip/demo/basic.tsx
+++ components/tooltip/demo/basic.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Tooltip } from 'antd';
 
 const App: React.FC = () => (
-  <Tooltip title="prompt text">
+  <Tooltip title="prompt text <p>fdafda</p>">
     <span>Tooltip will show on mouse enter.</span>
   </Tooltip>
 );
